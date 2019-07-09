39fd4382201fe06d3767aee9a9172968361e056a
MoriKen <p595201m@mail.kyutech.jp>
2019-07-09 23:52:38 +0900

    Fix codes

--- include/state_estimate_filter_ros/state_estimate_filter.h
+++ include/state_estimate_filter_ros/state_estimate_filter.h
@@ -2,9 +2,8 @@
 #define STATE_ESTIMATE_FILTER_H
 
 #include <ros/ros.h>
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <Eigen/Dense>
 
 namespace state_estimate_filter_ros
@@ -16,20 +15,12 @@ public:
   StateEstimateFilter(ros::NodeHandle& nh);
   virtual ~StateEstimateFilter();
 
-  void broadcast(void);
   void estimate(const Eigen::MatrixXd vec_input_curr);
   virtual void predict(const Eigen::MatrixXd vec_input_curr) = 0;
   virtual void filter(void) = 0;
 
 private:
-  void fixedPhoxiPoseCallback(const geometry_msgs::PoseStamped fixed_phoxi_pose);
-
-private:
   ros::NodeHandle nh_;
-  ros::Subscriber fixed_phoxi_sub_;
-  tf::Transform fixed_phoxi_tf_;
-  tf::TransformBroadcaster br_;
-  bool init_flg_;
 };
 }
 
--- src/state_estimate_filter.cpp
+++ src/state_estimate_filter.cpp
@@ -7,37 +7,12 @@ StateEstimateFilter::StateEstimateFilter()
 {
 }
 
-StateEstimateFilter::StateEstimateFilter(ros::NodeHandle& nh) : nh_(nh), init_flg_(false)
+StateEstimateFilter::StateEstimateFilter(ros::NodeHandle& nh) : nh_(nh)
 {
-  fixed_phoxi_sub_ = nh.subscribe("fixed_phoxi_pose", 1, &StateEstimateFilter::fixedPhoxiPoseCallback, this);
-}
-
-void StateEstimateFilter::broadcast(void)
-{
-  // Never broadcast tf unless at least once fixed_phoxi_pose is subscribed
-  if (!init_flg_)
-    return;
-
-  br_.sendTransform(tf::StampedTransform(fixed_phoxi_tf_, ros::Time::now(), "base_link", "fixed_phoxi_tf"));
 }
 
 void StateEstimateFilter::estimate(const Eigen::MatrixXd vec_input_curr)
 {
   this->predict(vec_input_curr);  // 一段先予測
   this->filter();                 // フィルタリング
-}
-
-void StateEstimateFilter::fixedPhoxiPoseCallback(const geometry_msgs::PoseStamped fixed_phoxi_pose)
-{
-  init_flg_ = true;
-
-  // Position
-  tf::Vector3 tf_vec =
-      tf::Vector3(fixed_phoxi_pose.pose.position.x, fixed_phoxi_pose.pose.position.y, fixed_phoxi_pose.pose.position.z);
-  fixed_phoxi_tf_.setOrigin(tf_vec);
-
-  // Posture
-  tf::Quaternion fixed_phoxi_quat(fixed_phoxi_pose.pose.orientation.x, fixed_phoxi_pose.pose.orientation.y,
-                                  fixed_phoxi_pose.pose.orientation.z, fixed_phoxi_pose.pose.orientation.w);
-  fixed_phoxi_tf_.setRotation(fixed_phoxi_quat);
 }
