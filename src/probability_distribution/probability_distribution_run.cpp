7d185d3936e6344e3ba470337e83bab477e159a0
MoriKen <p595201m@mail.kyutech.jp>
2019-07-11 03:32:18 +0900

    Modify executable

--- src/probability_distribution/probability_distribution_run.cpp
+++ src/probability_distribution/probability_distribution_run.cpp
@@ -1,13 +1,30 @@
 #include <probability_distribution/normal_distribution.h>
+#include <probability_distribution/depth_sensor_distribution.h>
 
 using probability_distribution::ProbabilityDistribution;
 using probability_distribution::NormalDistribution;
+using probability_distribution::DepthSensorDistribution;
 using Eigen::MatrixXd;
 
 // TEST CASES
-double run(void)
+double run_normal(void)
 {
   ProbabilityDistribution* prob_dis = new NormalDistribution();
+
+  MatrixXd input(1, 1);
+  input(0, 0) = 1.5;
+  MatrixXd output = prob_dis->calcDencityFunction(input);
+
+  double result = output(0, 0);
+
+  //const double ANSWER = 1.0;
+  //EXPECT_EQ(result, ANSWER);
+  return result;
+}
+
+double run_depth(void)
+{
+  ProbabilityDistribution* prob_dis = new DepthSensorDistribution();
 
   MatrixXd input(1, 1);
   input(0, 0) = 1.5;
@@ -26,8 +43,10 @@ int main(int argc, char** argv)
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  double ret = run();
-  std::cout << ret << std::endl;
+  double ret_normal = run_normal();
+  double ret_depth = run_depth();
+  std::cout << ret_normal << std::endl;
+  std::cout << ret_depth << std::endl;
   spinner.stop();
   ros::shutdown();
 
