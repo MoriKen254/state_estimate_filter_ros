56660732c3cd4110ca91a7d7e77b38ac0f299d56
MoriKen <p595201m@mail.kyutech.jp>
2019-07-10 23:15:14 +0900

    Add probability distribution

---
+++ include/probability_distribution/probability_distribution.h
@@ -0,0 +1,27 @@
+#ifndef PROBABILITY_DISTRIBUTION_H
+#define PROBABILITY_DISTRIBUTION_H
+
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <Eigen/Dense>
+
+namespace probability_distribution
+{
+class ProbabilityDistribution
+{
+public: // constructors & destoructors
+  ProbabilityDistribution();
+  virtual ~ProbabilityDistribution();
+
+public: // methods
+  virtual Eigen::MatrixXd calcDencityFunction(const Eigen::MatrixXd input) = 0;
+  virtual Eigen::MatrixXd calcLogDencityFunction(const Eigen::MatrixXd input) = 0;
+  Eigen::MatrixXd genRandomValue();
+
+private:
+
+};
+}  // namespace probability_distribution
+
+#endif  // PROBABILITY_DISTRIBUTION_H
