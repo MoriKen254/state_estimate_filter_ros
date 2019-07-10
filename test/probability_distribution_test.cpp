6e895abdafc6a12297494149010b8e8f79a25bc1
MoriKen <p595201m@mail.kyutech.jp>
2019-07-11 03:49:36 +0900

    Fix test

--- test/probability_distribution_test.cpp
+++ test/probability_distribution_test.cpp
@@ -5,6 +5,8 @@ using probability_distribution::ProbabilityDistribution;
 using probability_distribution::NormalDistribution;
 using Eigen::MatrixXd;
 
+const double EPS = 0.01;
+
 // TEST CASES
 TEST(ProbabilityDistributionTest, testNormal)
 {
@@ -16,8 +18,8 @@ TEST(ProbabilityDistributionTest, testNormal)
 
   double result = output(0, 0);
 
-  const double ANSWER = 1.0;
-  EXPECT_EQ(result, ANSWER);
+  const double ANSWER = 0.15056871607740221;
+  EXPECT_NEAR(result, ANSWER, EPS);
 }
 
 int main(int argc, char** argv)
