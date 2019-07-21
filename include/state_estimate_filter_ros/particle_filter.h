d44bef88286a1236f3b671b5980da9f80ac40cc8
MoriKen <p595201m@mail.kyutech.jp>
2019-07-21 17:26:44 +0900

    Add particle init

--- include/state_estimate_filter_ros/particle_filter.h
+++ include/state_estimate_filter_ros/particle_filter.h
@@ -2,6 +2,7 @@
 #define PARTICLE_FILTER_H
 
 #include "state_estimate_filter.h"
+#include <probability_distribution/normal_distribution.h>
 
 namespace state_estimate_filter_ros
 {
@@ -9,28 +10,37 @@ class Particle
 {
 public:
   Particle();
+  Particle(const Eigen::MatrixXd state, const Eigen::MatrixXd weight);
 
 public: // instead of property ...
   Eigen::MatrixXd state_;
   Eigen::MatrixXd weight_;
 };
 
-class ParticleFilter : StateEstimateFilter
+class ParticleFilter : public StateEstimateFilter
 {
 public:  // constructors & destoructors
   ParticleFilter();
   ParticleFilter(ros::NodeHandle& nh);
-  ParticleFilter(ros::NodeHandle& nh, const Eigen::MatrixXd system_a, const Eigen::MatrixXd system_b);
+  ParticleFilter(ros::NodeHandle& nh,
+                 const Eigen::MatrixXd system_a, const Eigen::MatrixXd system_b, const Eigen::MatrixXd system_c,
+                 const int num_particle, const Eigen::MatrixXd state, const Eigen::MatrixXd weight);
 
 public:                                                // methods
   void predict(const Eigen::MatrixXd input_curr);      // 予測ステップ(事前推定)
   void filter();                                       // フィルタリングステップ(事後推定)
 
 private:
+  void initParicles(const int num, const Eigen::MatrixXd state, const Eigen::MatrixXd weight);
+
+private:
   Eigen::MatrixXd system_a_;
   Eigen::MatrixXd system_b_;
-  Eigen::MatrixXd predicted_val_;
+  Eigen::MatrixXd system_c_;
   std::vector<Particle> particles_;
+  std::default_random_engine gen_system_;
+  std::normal_distribution<double> dist_system_;
+  probability_distribution::ProbabilityDistribution* prob_dis_;
 };
 }
 
--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -1,11 +1,20 @@
 #include <state_estimate_filter_ros/particle_filter.h>
 
-using state_estimate_filter_ros::ParticleFilter;
-using state_estimate_filter_ros::Particle;
+using dist_type = std::normal_distribution<>;
 using Eigen::MatrixXd;
+
+using state_estimate_filter_ros::Particle;
+using state_estimate_filter_ros::ParticleFilter;
+using probability_distribution::NormalDistribution;
+using probability_distribution::ProbabilityDistribution;
 
 // Class methods definitions
 Particle::Particle() : state_(MatrixXd::Zero(0, 0)), weight_(MatrixXd::Zero(0, 0))
+{
+}
+
+Particle::Particle(MatrixXd state, MatrixXd weight)
+  : state_(state), weight_(weight)
 {
 }
 
@@ -17,16 +26,38 @@ ParticleFilter::ParticleFilter(ros::NodeHandle& nh) : StateEstimateFilter(nh)
 {
 }
 
-ParticleFilter::ParticleFilter(ros::NodeHandle& nh, const Eigen::MatrixXd system_a, const Eigen::MatrixXd system_b)
-  : StateEstimateFilter(nh), system_a_(system_a), system_b_(system_b)
+ParticleFilter::ParticleFilter(ros::NodeHandle& nh,
+                               const MatrixXd system_a, const MatrixXd system_b, const MatrixXd system_c,
+                               const int num_particle, const MatrixXd state, const MatrixXd weight)
+  : StateEstimateFilter(nh), system_a_(system_a), system_b_(system_b), system_c_(system_c)
 {
+  gen_system_.seed();
+  dist_type::param_type param(0.0, 1.0);
+  dist_system_.param(param);
+
+  initParicles(num_particle, state, weight);
 }
 
-void ParticleFilter::predict(const Eigen::MatrixXd input_curr)
+void ParticleFilter::initParicles(const int num, const MatrixXd state, const MatrixXd weight)
 {
-  predicted_val_ = system_a_ * input_curr + system_b_;
+  for(int i = 0; i < num; i++)
+  {
+    particles_.push_back(Particle(state, weight));
+  }
+  int a = 0;
+
+}
+
+void ParticleFilter::predict(const MatrixXd input_curr)
+{
+  MatrixXd noise(1, 1);
+  noise(0, 0) = dist_system_(gen_system_);
+  vec_predict_curr_ = system_a_ * vec_estimate_prev_ + system_b_ * input_curr + system_c_ + noise;
 }
 
 void ParticleFilter::filter(void)
 {
+  vec_estimate_curr_ = vec_predict_curr_;
+  vec_estimate_prev_ = vec_estimate_curr_;
+  vec_predict_prev_ = vec_predict_curr_;
 }
